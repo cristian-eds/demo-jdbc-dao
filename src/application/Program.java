489448111ac4bb18c4eac5a3ef31e01f1e1f67d5
Cristian Eduardo dos Santos <cristian.santos0308@gmail.com>
2022-10-20 21:41:39 -0300

    Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department depart = new Department(1,"Eletronics");
+		
+		Seller seller = new Seller(1, "Fulano","fulano@email.com",new Date(),2000.0,depart);
 		System.out.println(depart);
+		System.out.println(seller);
 
 	}
 
